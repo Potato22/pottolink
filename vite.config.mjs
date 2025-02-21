885c629f38cd2441767a9e2906c25f7e6dcfb08c
Potato22 <potatooosss22@gmail.com>
2025-02-21 20:25:34 +0700

    panic

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import path from 'path';
 
 export default defineConfig({
   root: '.',
   base: './',
+  resolve: {
+    alias: {
+      'jquery': path.resolve(__dirname, 'node_modules/jquery/dist/jquery.min.js')
+    }
+  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
